f6a39174a49e94d8aca68d8b54bc332ad39faaaa
MLaminekane <94493308+MLaminekane@users.noreply.github.com>
2022-03-07 17:51:38 +0000

    bases

---
+++ Apps/apps.py
@@ -0,0 +1,7 @@
+dayOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"]
+dayOfWeek.append("samedi")
+print(dayOfWeek)
+dayOfWeek.pop()
+print(dayOfWeek)
+dayOfWeek.append("Dimanche")
+print(dayOfWeek)
