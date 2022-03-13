48085f3c4348e47c4662c99586467006ebdb4fd5
MLaminekane <94493308+MLaminekane@users.noreply.github.com>
2022-03-13 20:07:16 +0000

    bases

--- Apps/apps.py
+++ Apps/apps.py
@@ -1,4 +1,18 @@
-dayOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"]
-dayOfWeek.append("samedi")
-dayOfWeek.append("Dimanche")
-len(dayOfWeek)
+# # for i in range(10):
+# #     print(i * '*')
+
+# # a = input("entrer a: ")
+# # b = input("entrer b: ")
+# # print(f"résultat: {int(a)+int(b)} ")
+
+# age = input('votre age: ')
+# age = int(age)
+# if age >= 18:
+#     print("vous etes majeur")
+# else:
+#     print("vous etes mineur")    
+
+# import os
+# chemin = "/Users/laminekane/Documents"
+# dossier = os.path.join(chemin, "dossier", "test") 
+# os.makedirs(dossier)
