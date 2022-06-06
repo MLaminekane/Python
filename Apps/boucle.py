46d5f95dc29764a34b994a062b277b06c1331b81
Lamine Kane <mlaminekane01@gmail.com>
2022-06-06 19:17:21 +0000

    exo loop

---
+++ Apps/boucle.py
@@ -0,0 +1,43 @@
+# liste = [1, 2, 3, 4, 5]
+# for liste in range(1, 10):
+#     if liste == 3:
+#         print('found')
+#         break
+#     print(liste)
+
+# liste = [1, 2, 3, 4, 5]
+# for liste in range(1, 10):
+#     if liste == 3:
+#         print('found')
+#         continue
+#     print(liste)
+
+# liste = [1, 2, 3, 4, 5]
+# for liste in range(1, 10):
+#     for letter in 'abc':
+#         print(liste, letter)
+
+# x = 0
+# while x <10:
+#     if x == 5:
+#         print('yes')
+#         break
+#     print(x)
+#     x += 1
+
+# x = 0
+# while True:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
+
+i = 1
+while i <= 5:
+    print('lamine', end=" ")
+    j = 1 
+    while j <4:
+        print("kane ", end="")
+        j = j + 1
+    i += 1
+    print()
